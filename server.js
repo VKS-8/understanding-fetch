e2693ddaae8870a1b93e5e2b06fb01c9fbea2897
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-07-23 12:03:39 -0500

    Add dotenv of .env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const port = 5500
 
 const app = express();
