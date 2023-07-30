d26ff13c64fbbb05211d720379370ea7f4e551e4
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-07-29 20:50:23 -0500

    Major revamp to update code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http');
 const https = require('https');
+require('dotenv').config();
 // const path = require('path');
 const port = 5501
 
@@ -20,11 +21,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Initialize the app
 app.use(express.static('website'));
 
+// Global variables
+const apiKey = process.env.API_KEY;
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+
 // This hard coded api url does fetch the weather data object from open weather map
 async function getData () {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=10012,US&units=imperial&appid=9a6f38cd817b93e366a58123f0a05b6e');
+  const response = await fetch(`${baseUrl}zip=10012,US&units=imperial&appid=${apiKey}`);
   const responseData = await response.json();
   console.log(responseData);
+  return responseData;
 }
 
 // Test api with hard coded params
@@ -35,8 +41,48 @@ getData();
 // The Net Ninja
 // Node.js Crash Course #6 - Express Apps
 
-app.get('/', (req, res) => {
-  res.sendFile('./website/index.html', { root: __dirname });
+// app.get('/', (req, res) => {
+//   res.sendFile('./website/index.html', { root: __dirname });
+// });
+
+// The following POST code was the help of chatGPT
+app.post('/clientInput', async (req, res) => {
+  const {zip, country, units, feelings } = req.body;
+  console.log(req.body);
+
+  const apiUrl = `${baseUrl}zip=${zip},${country}&units=${units}&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from API');
+    }
+
+  const weatherData = await response.json();
+
+  const currentDate = new Date();
+  const formatDate = currentDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  const combinedData = {
+    currentDate: formatDate,
+    temp: weatherData.main.temp,
+    name: weatherData.name,
+    feelings: feelings
+  }
+
+  console.log(combinedData);
+
+  res.json(combinedData);
+
+} catch (error) {
+  console.error('Error making request to API', error);
+  res.status(500).json({ error: "Failed to fetch data from API" });
+}
 });
 
 // 404 page per this YouTube tutorial:
@@ -45,43 +91,6 @@ app.get('/', (req, res) => {
 // Node.js Crash Course #6 - Express Apps
 app.use((req, res) => {
   res.status(404).sendFile('./website/404.html', { root: __dirname});
-});
-
-// The following POST code was the help of chatGPT
-app.post('/clientInput', (req, res) => {
-  const { zip, country, units } = req.body;
-  console.log(req.body);
-
-  // Parse the URL to get the necessary components for the HTTP/HTTPS request
-  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&units=${units}&appid=9a6f38cd817b93e366a58123f0a05b6e`;
-  const url = new URL(apiUrl);
-  const protocol = url.protocol === "https:" ? https : http;
-  const options = {
-    hostname: url.hostname,
-    port: url.port || (url.protocol === "https:" ? 443 : 80 ),
-    path: url.pathname + url.search,
-    method: "GET",
-  };
-
-  // Make the HTTP/HTTPS request to the external API
-  const request = protocol.request(options, (response) => {
-    let data = "";
-
-    response.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    response.on("end", () => {
-      res.json(JSON.parse(data));
-    });
-  });
-
-  request.on("error", (error) => {
-    console.error("Error making request to API", error);
-    res.status(500).json({ error: "Failed to fetch data from API" });
-  })
-
-  request.end();
 });
 
 // Setup Server
