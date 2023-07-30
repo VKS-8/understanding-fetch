5a338ec6f6eef9c33482cf51b9ef24ef3290758c
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-07-29 20:52:07 -0500

    Update code

--- website/app.js
+++ website/app.js
@@ -1,3 +1,5 @@
+const serverUrl = 'http://localhost:5501'
+
 // Used chatGPT to figure out how to set the country code to uppercase in the form
 // as soon as the user entered
 // Function to convert the input value to upper case
@@ -9,32 +11,47 @@ function convertToUpperCase() {
 // Add an event listener to the input field to handle input changes
 document.getElementById("country").addEventListener("input", convertToUpperCase);
 
+async function fetchData(endpoint, data) {
+  try {
+    const response = await fetch(`/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
+}
+
 
 // Used chatGPT to determine how to set up proper parameters from form to
 // post to server
-const form = document.getElementById('clientForm');
-form.addEventListener('submit', async (e) => {
+document.getElementById('clientInput').addEventListener('submit', async (e) => {
   e.preventDefault();
-  const formData = new FormData(form);
-  const { zip, country, units } = Object.fromEntries(formData);
+
+  const formData = new FormData(e.target);
+  const userData = Object.fromEntries(formData.entries());
 
   try {
-    const response = await fetch('http://localhost:5501/clientInput', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ zip, country, units }),
-    });
+    const serverData = await fetchData('/clientInput', userData);
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Response from server:", data);
-    } else {
-      console.error("Error from server:", response.status);
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
     }
-  } catch (error) {
-    console.error("Error:", error);
+      serverData = await response.json();
+      console.log("Response from server:", serverData);
+    } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
   }
-});
+})();
 
